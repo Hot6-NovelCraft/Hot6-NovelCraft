2dd91384c62c4e8de8987ea6527d58d1579b21af
Jeon Min Woo <wjsalsdn120@naver.com>
2026-05-13 20:59:21 +0900

    fix : 캘린더 수정

--- src/main/java/com/example/hot6novelcraft/common/exception/domain/NationalLibraryExceptionEnum.java
+++ src/main/java/com/example/hot6novelcraft/common/exception/domain/NationalLibraryExceptionEnum.java
@@ -13,7 +13,8 @@ public enum NationalLibraryExceptionEnum implements ErrorCode {
     BOOK_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 저장된 도서입니다"),
     EXTERNAL_API_ERROR(HttpStatus.BAD_GATEWAY, "국립중앙도서관 API 호출 중 오류가 발생했습니다"),
     INVALID_ISBN(HttpStatus.BAD_REQUEST, "유효하지 않은 ISBN입니다"),
-    BOOK_ALREADY_IN_SHELF(HttpStatus.CONFLICT, "이미 내 서재에 저장된 도서입니다");
+    BOOK_ALREADY_IN_SHELF(HttpStatus.CONFLICT, "이미 내 서재에 저장된 도서입니다"),
+    BOOK_NOT_IN_SHELF(HttpStatus.NOT_FOUND, "내 서재에 없는 도서입니다");
 
 
     private final HttpStatus httpStatus;
