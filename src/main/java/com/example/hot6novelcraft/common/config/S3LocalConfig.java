dfa101a2ad0f5e723a9b38e16096460e360de2d8
MinWoo1995 <wjsalsdn120@naver.com>
2026-09-06 18:53:59 +0900

    feat: 배포 환경변수 주입 및 S3 IAM Role 분기 설정
    - S3LocalConfig: @Profile(local) StaticCredentialsProvider 신규 추가 (로컬 개발용)
    - docker-compose.yml: app 서비스 환경변수 주입, redis-net subnet 고정, redis-master IP 고정
    - deploy.yml: .env 파일 S3 경유 전달 방식으로 변경
    - .gitignore: application-local.yml 추가

--- src/main/java/com/example/hot6novelcraft/common/config/S3LocalConfig.java
+++ src/main/java/com/example/hot6novelcraft/common/config/S3LocalConfig.java
@@ -4,31 +4,40 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Profile("!local")
-public class S3Config {
+@Profile("local")
+public class S3LocalConfig {
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 
     @Bean
     public S3Presigner s3Presigner() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
